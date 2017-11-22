e13f012c0b37b0ca95f2d7811189b29dedb21dca
MARCO ANTONIO MAMANI CHURA <MARCOANTONIOMAMANICHURA@GMAIL.COM>
2017-11-22 09:26:37 -0400

    fddsfdsffdsdf

--- controllers/orders.js
+++ controllers/orders.js
@@ -10,6 +10,21 @@ function postPedidos(req, res){
     const token =req.headers.tokenauthorization
     const payload=jwt.decode(token,config.SECRET_TOKEN)
     const cod_cliente=payload.sub
+    const listajson=req.body
+    
+    for (var i = 0; i < listajson.length; i++) {    
+       const item=listajson[i]
+        
+
+        if(Number.isInteger(item["cantidad"])){
+            
+        }else{
+            res.status(200).send({code:4,message:'Los datos de la cantidad son invalido. Por favor coloque datos numericos'})
+         return   
+        }
+    }; 
+
+
 
     const detalle=js2xmlparser.parse("row", req.body)
     //console.log(detalle)
